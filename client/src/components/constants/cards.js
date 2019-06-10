25ea1d526850d8e21922c06b8694ff545b01f37e
Aaron Mednick <aaronjosephm@gmail.com>
2019-06-09 18:28:09 -0700

    added more code

--- client/src/components/constants/cards.js
+++ client/src/components/constants/cards.js
@@ -1,3 +1,56 @@
+import TWO_OF_HEARTS_IMG from '../../images/playing_cards/2H.png';
+import THREE_OF_HEARTS_IMG from '../../images/playing_cards/3H.png';
+import FOUR_OF_HEARTS_IMG from '../../images/playing_cards/4H.png';
+import FIVE_OF_HEARTS_IMG from '../../images/playing_cards/5H.png';
+import SIX_OF_HEARTS_IMG from '../../images/playing_cards/6H.png';
+import SEVEN_OF_HEARTS_IMG from '../../images/playing_cards/7H.png';
+import EIGHT_OF_HEARTS_IMG from '../../images/playing_cards/8H.png';
+import NINE_OF_HEARTS_IMG from '../../images/playing_cards/9H.png';
+import TEN_OF_HEARTS_IMG from '../../images/playing_cards/10H.png';
+import JACK_OF_HEARTS_IMG from '../../images/playing_cards/JH.png';
+import QUEEN_OF_HEARTS_IMG from '../../images/playing_cards/QH.png';
+import KING_OF_HEARTS_IMG from '../../images/playing_cards/KH.png';
+import ACE_OF_HEARTS_IMG from '../../images/playing_cards/AH.png';
+import TWO_OF_CLUBS_IMG from '../../images/playing_cards/2C.png';
+import THREE_OF_CLUBS_IMG from '../../images/playing_cards/3C.png';
+import FOUR_OF_CLUBS_IMG from '../../images/playing_cards/4C.png';
+import FIVE_OF_CLUBS_IMG from '../../images/playing_cards/5C.png';
+import SIX_OF_CLUBS_IMG from '../../images/playing_cards/6C.png';
+import SEVEN_OF_CLUBS_IMG from '../../images/playing_cards/7C.png';
+import EIGHT_OF_CLUBS_IMG from '../../images/playing_cards/8C.png';
+import NINE_OF_CLUBS_IMG from '../../images/playing_cards/9C.png';
+import TEN_OF_CLUBS_IMG from '../../images/playing_cards/10C.png';
+import JACK_OF_CLUBS_IMG from '../../images/playing_cards/JC.png';
+import QUEEN_OF_CLUBS_IMG from '../../images/playing_cards/QC.png';
+import KING_OF_CLUBS_IMG from '../../images/playing_cards/KC.png';
+import ACE_OF_CLUBS_IMG from '../../images/playing_cards/AC.png';
+import TWO_OF_DIAMONDS_IMG from '../../images/playing_cards/2D.png';
+import THREE_OF_DIAMONDS_IMG from '../../images/playing_cards/3D.png';
+import FOUR_OF_DIAMONDS_IMG from '../../images/playing_cards/4D.png';
+import FIVE_OF_DIAMONDS_IMG from '../../images/playing_cards/5D.png';
+import SIX_OF_DIAMONDS_IMG from '../../images/playing_cards/6D.png';
+import SEVEN_OF_DIAMONDS_IMG from '../../images/playing_cards/7D.png';
+import EIGHT_OF_DIAMONDS_IMG from '../../images/playing_cards/8D.png';
+import NINE_OF_DIAMONDS_IMG from '../../images/playing_cards/9D.png';
+import TEN_OF_DIAMONDS_IMG from '../../images/playing_cards/10D.png';
+import JACK_OF_DIAMONDS_IMG from '../../images/playing_cards/JD.png';
+import QUEEN_OF_DIAMONDS_IMG from '../../images/playing_cards/QD.png';
+import KING_OF_DIAMONDS_IMG from '../../images/playing_cards/KD.png';
+import ACE_OF_DIAMONDS_IMG from '../../images/playing_cards/AD.png';
+import TWO_OF_SPAIDS_IMG from '../../images/playing_cards/2S.png';
+import THREE_OF_SPAIDS_IMG from '../../images/playing_cards/3S.png';
+import FOUR_OF_SPAIDS_IMG from '../../images/playing_cards/4S.png';
+import FIVE_OF_SPAIDS_IMG from '../../images/playing_cards/5S.png';
+import SIX_OF_SPAIDS_IMG from '../../images/playing_cards/6S.png';
+import SEVEN_OF_SPAIDS_IMG from '../../images/playing_cards/7S.png';
+import EIGHT_OF_SPAIDS_IMG from '../../images/playing_cards/8S.png';
+import NINE_OF_SPAIDS_IMG from '../../images/playing_cards/9S.png';
+import TEN_OF_SPAIDS_IMG from '../../images/playing_cards/10S.png';
+import JACK_OF_SPAIDS_IMG from '../../images/playing_cards/JS.png';
+import QUEEN_OF_SPAIDS_IMG from '../../images/playing_cards/QS.png';
+import KING_OF_SPAIDS_IMG from '../../images/playing_cards/KS.png';
+import ACE_OF_SPAIDS_IMG from '../../images/playing_cards/AS.png';
+
 export const TWO_OF_HEARTS = '2H';
 export const THREE_OF_HEARTS = '3H';
 export const FOUR_OF_HEARTS = '4H';
@@ -26,6 +79,20 @@ export const QUEEN_OF_CLUBS = 'QC';
 export const KING_OF_CLUBS = 'KC';
 export const ACE_OF_CLUBS = 'AC';
 
+export const TWO_OF_DIAMONDS = '2D';
+export const THREE_OF_DIAMONDS = '3D';
+export const FOUR_OF_DIAMONDS = '4D';
+export const FIVE_OF_DIAMONDS = '5D';
+export const SIX_OF_DIAMONDS = '6D';
+export const SEVEN_OF_DIAMONDS = '7D';
+export const EIGHT_OF_DIAMONDS = '8D';
+export const NINE_OF_DIAMONDS = '9D';
+export const TEN_OF_DIAMONDS = '10D';
+export const JACK_OF_DIAMONDS = 'JD';
+export const QUEEN_OF_DIAMONDS = 'QD';
+export const KING_OF_DIAMONDS = 'KD';
+export const ACE_OF_DIAMONDS = 'AD';
+
 export const TWO_OF_SPAIDS = '2S';
 export const THREE_OF_SPAIDS = '3S';
 export const FOUR_OF_SPAIDS = '4S';
@@ -40,16 +107,112 @@ export const QUEEN_OF_SPAIDS = 'QS';
 export const KING_OF_SPAIDS = 'KS';
 export const ACE_OF_SPAIDS = 'AS';
 
-export const TWO_OF_DIAMONDS = '2D';
-export const THREE_OF_DIAMONDS = '3D';
-export const FOUR_OF_DIAMONDS = '4D';
-export const FIVE_OF_DIAMONDS = '5D';
-export const SIX_OF_DIAMONDS = '6D';
-export const SEVEN_OF_DIAMONDS = '7D';
-export const EIGHT_OF_DIAMONDS = '8D';
-export const NINE_OF_DIAMONDS = '9D';
-export const TEN_OF_DIAMONDS = '10D';
-export const JACK_OF_DIAMONDS = 'JD';
-export const QUEEN_OF_DIAMONDS = 'QD';
-export const KING_OF_DIAMONDS = 'KD';
-export const ACE_OF_DIAMONDS = 'AD';
+export const cards = [
+	TWO_OF_HEARTS,
+	THREE_OF_HEARTS,
+	FOUR_OF_HEARTS,
+	FIVE_OF_HEARTS,
+	SIX_OF_HEARTS,
+	SEVEN_OF_HEARTS,
+	EIGHT_OF_HEARTS,
+	NINE_OF_HEARTS,
+	TEN_OF_HEARTS,
+	JACK_OF_HEARTS,
+	QUEEN_OF_HEARTS,
+	KING_OF_HEARTS,
+	ACE_OF_HEARTS,
+	TWO_OF_CLUBS,
+	THREE_OF_CLUBS,
+	FOUR_OF_CLUBS,
+	FIVE_OF_CLUBS,
+	SIX_OF_CLUBS,
+	SEVEN_OF_CLUBS,
+	EIGHT_OF_CLUBS,
+	NINE_OF_CLUBS,
+	TEN_OF_CLUBS,
+	JACK_OF_CLUBS,
+	QUEEN_OF_CLUBS,
+	KING_OF_CLUBS,
+	ACE_OF_CLUBS,
+	TWO_OF_DIAMONDS,
+	THREE_OF_DIAMONDS,
+	FOUR_OF_DIAMONDS,
+	FIVE_OF_DIAMONDS,
+	SIX_OF_DIAMONDS,
+	SEVEN_OF_DIAMONDS,
+	EIGHT_OF_DIAMONDS,
+	NINE_OF_DIAMONDS,
+	TEN_OF_DIAMONDS,
+	JACK_OF_DIAMONDS,
+	QUEEN_OF_DIAMONDS,
+	KING_OF_DIAMONDS,
+	ACE_OF_DIAMONDS,
+	TWO_OF_SPAIDS,
+	THREE_OF_SPAIDS,
+	FOUR_OF_SPAIDS,
+	FIVE_OF_SPAIDS,
+	SIX_OF_SPAIDS,
+	SEVEN_OF_SPAIDS,
+	EIGHT_OF_SPAIDS,
+	NINE_OF_SPAIDS,
+	TEN_OF_SPAIDS,
+	JACK_OF_SPAIDS,
+	QUEEN_OF_SPAIDS,
+	KING_OF_SPAIDS,
+	ACE_OF_SPAIDS
+];
+
+export const cardImages = {
+	'2H': TWO_OF_HEARTS_IMG,
+	'3H': THREE_OF_HEARTS_IMG,
+	'4H': FOUR_OF_HEARTS_IMG,
+	'5H': FIVE_OF_HEARTS_IMG,
+	'6H': SIX_OF_HEARTS_IMG,
+	'7H': SEVEN_OF_HEARTS_IMG,
+	'8H': EIGHT_OF_HEARTS_IMG,
+	'9H': NINE_OF_HEARTS_IMG,
+	'10H': TEN_OF_HEARTS_IMG,
+	'JH': JACK_OF_HEARTS_IMG,
+	'QH': QUEEN_OF_HEARTS_IMG,
+	'KH': KING_OF_HEARTS_IMG,
+	'AH': ACE_OF_HEARTS_IMG,
+	'2C': TWO_OF_CLUBS_IMG,
+	'3C': THREE_OF_CLUBS_IMG,
+	'4C': FOUR_OF_CLUBS_IMG,
+	'5C': FIVE_OF_CLUBS_IMG,
+	'6C': SIX_OF_CLUBS_IMG,
+	'7C': SEVEN_OF_CLUBS_IMG,
+	'8C': EIGHT_OF_CLUBS_IMG,
+	'9C': NINE_OF_CLUBS_IMG,
+	'10C': TEN_OF_CLUBS_IMG,
+	'JC': JACK_OF_CLUBS_IMG,
+	'QC': QUEEN_OF_CLUBS_IMG,
+	'KC': KING_OF_CLUBS_IMG,
+	'AC': ACE_OF_CLUBS_IMG,
+	'2D': TWO_OF_DIAMONDS_IMG,
+	'3D': THREE_OF_DIAMONDS_IMG,
+	'4D': FOUR_OF_DIAMONDS_IMG,
+	'5D': FIVE_OF_DIAMONDS_IMG,
+	'6D': SIX_OF_DIAMONDS_IMG,
+	'7D': SEVEN_OF_DIAMONDS_IMG,
+	'8D': EIGHT_OF_DIAMONDS_IMG,
+	'9D': NINE_OF_DIAMONDS_IMG,
+	'10D': TEN_OF_DIAMONDS_IMG,
+	'JD': JACK_OF_DIAMONDS_IMG,
+	'QD': QUEEN_OF_DIAMONDS_IMG,
+	'KD': KING_OF_DIAMONDS_IMG,
+	'AD': ACE_OF_DIAMONDS_IMG,
+	'2S': TWO_OF_SPAIDS_IMG,
+	'3S': THREE_OF_SPAIDS_IMG,
+	'4S': FOUR_OF_SPAIDS_IMG,
+	'5S': FIVE_OF_SPAIDS_IMG,
+	'6S': SIX_OF_SPAIDS_IMG,
+	'7S': SEVEN_OF_SPAIDS_IMG,
+	'8S': EIGHT_OF_SPAIDS_IMG,
+	'9S': NINE_OF_SPAIDS_IMG,
+	'10S': TEN_OF_SPAIDS_IMG,
+	'JS': JACK_OF_SPAIDS_IMG,
+	'QS': QUEEN_OF_SPAIDS_IMG,
+	'KS': KING_OF_SPAIDS_IMG,
+	'AS': ACE_OF_SPAIDS_IMG
+};
--- client/src/components/gameboard/Canvas.js
+++ client/src/components/gameboard/Canvas.js
@@ -4,26 +4,99 @@ import { connect } from "react-redux";
 import styled from 'styled-components';
 import Table from '../../images/poker-table.png';
 import { cards } from '../constants/cards';
+import { cardImages } from '../constants/cards';
 
 class Canvas extends Component {
+  constructor() {
+    super();
+    this.state = {
+    	deck: cards,
+      dealerHand: {},
+      playerHand: {}
+    }
+  }
 
-  generateRandomCard() {
+  componentWillMount() {
+    if (this.state.deck === []) {
+      this.setState({ deck: cards })
+    }
+  }
 
+  generateRandomCard = () => {
+  	const deck = Object.assign([], this.state.deck);
+    console.log("deck in generateRandomCard: ", deck);
+  	const currentLength = deck.length - 1;
+  	const pos = Math.floor(Math.random()*currentLength);
+  	const card = deck[pos];
+
+    console.log("card: ", card);
+  	deck.splice(pos, 1);
+    this.setState({ deck });
+  	return card;
+  }
+
+  generateDealerHand = () => {
+    const leftCard = this.generateRandomCard();
+    const rightCard = this.generateRandomCard();
+    const dealerHand = {
+      leftCard: leftCard,
+      rightCard: rightCard 
+    }
+    console.log("dealerHand: ", dealerHand);
+    this.setState({ dealerHand });
+  }
+
+  generatePlayerHand = () => {
+    const leftCard = this.generateRandomCard();
+    const rightCard = this.generateRandomCard();
+    const playerHand = {
+      leftCard: leftCard,
+      rightCard: rightCard 
+    }
+    console.log("playerHand: ", playerHand);
+    this.setState({ playerHand });
+    console.log("state: ", this.state);
+  }
+
+  play = async e => {
+    e.preventDefault();
+    console.log("cards: ", cards);
+    await this.setState({ deck: cards, dealerHand: {}, playerHand: {}});
+    console.log("deck: ", this.state.deck);
+    this.generatePlayerHand();
+    this.generateDealerHand();
   }
   
   render() {
+    const playerCards = this.state.playerHand;
+    const hand = Object.values(playerCards).map(card => {
+      console.log("here is that value: ", card);
+      const cardImg = cardImages[`${card}`];
+      return (
+        <span>
+          <Card>
+            <img src={cardImg} width='50' height='80' />
+          </Card>
+        </span>
+        );
+    })
   	console.log('table is: ', Table);
     return (
       <div>
         <h1>Welcome to a new game!</h1>
         <HandDescription>
           <h3>Your hand:</h3>
-          <Hand>
-          	<RandomCard />
-          	<RandomCard />
-          </Hand>
+          <PlayerHand>
+            {hand}
+          </PlayerHand>
         </HandDescription>
         <img src={Table} alt="poker table" height='400' width='700'/>
+        <PlayButton
+            className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+            onClick={(e) => this.play(e)}
+          >
+            Play
+        </PlayButton>
       </div>
     );
   }
@@ -31,8 +104,25 @@ class Canvas extends Component {
 
 export default Canvas;
 
+const PlayerHand = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: space-around;
+  bottom: -85px;
+  z-index: 5;
+  width: 150px;
+  height: 100px;
+  background-color: red;
+`;
+
+const Card = styled.div`
+  width: 50px;
+  height: 80px;
+  background-color: blue;
+`
+
 const HandDescription = styled.div`
-    width: 800px;
+  width: 800px;
 	position: absolute;
 	padding-top: 200px;
 	display: flex;
@@ -41,6 +131,14 @@ const HandDescription = styled.div`
 	& h3 {
 		color: white !important;
 	}
+`;
+
+const PlayButton = styled.button`
+  position: relative;
+  width: 200px;
+  borderRadius: 3px;
+  letterSpacing: 1.5px;
+  marginTop: 1rem;
 `;
 
 
