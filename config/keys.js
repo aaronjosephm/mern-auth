91585341b5e78c62d1e5446343b571be539f5b52
Aaron Mednick <aaronjosephm@gmail.com>
2019-07-23 15:09:58 -0300

    testing this

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-module.exports = {
-  mongoURI: "mongodb://aaronjosephm:deadwolf22@ds159110.mlab.com:59110/first_mongo_db",
-  secretOrKey: "secret" 
-};
+// module.exports = {
+//   mongoURI: "mongodb://aaronjosephm:deadwolf22@ds159110.mlab.com:59110/first_mongo_db",
+//   secretOrKey: "secret" 
+// };
